4f339aac1c53392501e819f526a10610c715d1fa
Cezar Castro Rosa <cecez86@gmail.com>
2022-01-17 09:58:44 -0300

    docs: contar caracteres em string e multiline string

--- 001-swift-4.2-novas-features-e-melhorias-nas-apis/001-alura-swift-4-2/001-alura-swift-4-2/02-strings/01-colecoes.playground.playground/Contents.swift
+++ 001-swift-4.2-novas-features-e-melhorias-nas-apis/001-alura-swift-4-2/001-alura-swift-4-2/02-strings/01-colecoes.playground.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-var texto = "Uma string é um array de caracteres.!!!"
+var texto = "Uma string é um array/coleção de caracteres.!!!"
 
 print(type(of: texto))
 
@@ -16,4 +16,24 @@ texto.forEach { print($0, separator: "", terminator: "-") }
  U-m-a- -s-t-r-i-n-g- -é- -u-m- -a-r-r-a-y- -d-e- -c-a-r-a-c-t-e-r-e-s-.-!-!-!-
  
  
+ */
+
+let mensagem = "Bla bla bla13"
+print("\n" + String(mensagem.count))
+
+/*
+ Retorno: 13
+ */
+
+let mensagemMultiLineString = """
+Loucura total
+quebrando linhas
+na string sem colocar barra n
+"""
+print(mensagemMultiLineString)
+/*
+ Retorno:
+ Loucura total
+ quebrando linhas
+ na string sem colocar barra n
  */
